c6724e86f194f4128ea26cfc5c582382bf622c6d
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-11-26 16:28:35 +0530

    updated vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   root: './',
+//   build: {
+//     outDir: 'dist',
+//   },
+//   publicDir: 'public'
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  root: './',
+  root: "./",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
+    chunkSizeWarningLimit: 1500, // Increase limit to 1.5MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendor: [
+            "axios",
+            "lodash.debounce",
+            "date-fns",
+            "swiper",
+            "xlsx",
+            "@tanstack/react-query",
+            "framer-motion",
+          ],
+        },
+      },
+    },
   },
-  publicDir: 'public'
-})
+  publicDir: "public",
+});

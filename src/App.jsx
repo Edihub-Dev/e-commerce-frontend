07178fe0376ddc59b70af57975e59560b0e5401b
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-12-21 12:01:09 +0530

    coupon page added in seller dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ function App() {
               path="orders/:orderId"
               element={<SellerOrderDetailsPage />}
             />
-            {/* <Route path="coupons" element={<SellerCoupons />} /> */}
+            <Route path="coupons" element={<SellerCoupons />} />
           </Route>
           <Route
             path="/admin"

5e1930584d585519648967e32d7cf6a7d7e604d7
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-12-26 10:41:13 +0530

    email send for admin from seller side is toggle true and false foramte added

--- src/store/slices/checkoutSlice.js
+++ src/store/slices/checkoutSlice.js
@@ -9,7 +9,7 @@ const initialState = {
   orderId: null,
   appliedCoupon: null,
   qrfolioUpload: null,
-  qrfolioRequired: false,
+  qrfolioRequired: true,
   totals: {
     subtotal: 0,
     shippingFee: 0,

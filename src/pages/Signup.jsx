dc1c1bb35fc59fdb24269e1c130e85cc59875440
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-12-30 14:33:52 +0530

    sign up page fix

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -82,7 +82,7 @@ const Signup = () => {
               htmlFor="username"
               className="block text-sm font-medium text-gray-700"
             >
-              FullName
+              Full Name
             </label>
             <input
               type="text"
